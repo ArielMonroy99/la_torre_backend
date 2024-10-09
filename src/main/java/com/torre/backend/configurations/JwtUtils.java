9a7cd3f6260c35d26538fa3c948325fe7f96d0dc
Ariel Monroy <arimonmon999@gmail.com>
2024-10-08 21:50:40 -0400

    feat: adds intial configuration

---
+++ src/main/java/com/torre/backend/configurations/JwtUtils.java
@@ -0,0 +1,10 @@
+package com.torre.backend.configurations;
+
+import io.jsonwebtoken.Claims;
+
+public class JwtUtils {
+    public static String extractUsername(String token) {
+        String subject = "";
+        return subject;
+    }
+}

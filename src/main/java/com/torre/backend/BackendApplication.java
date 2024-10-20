5a9a44f1a3c690ac49981feb51e42d2a639b0d6f
Ariel Monroy <arimonmon999@gmail.com>
2024-10-20 12:44:36 -0400

    fix: enables jpa auditing in app main

--- src/main/java/com/torre/backend/BackendApplication.java
+++ src/main/java/com/torre/backend/BackendApplication.java
@@ -3,8 +3,11 @@ package com.torre.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(exclude = { ErrorMvcAutoConfiguration.class })
+@EnableJpaAuditing
 public class BackendApplication {
 
 	public static void main(String[] args) {
